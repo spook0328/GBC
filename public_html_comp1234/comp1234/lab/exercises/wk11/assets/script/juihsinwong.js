c7d459e90fb5250bf29d82d44c79dd06e80d3f53
Eric <eric032807@gmial.com>
2024-11-13 15:30:07 -0500

    wk11 50%work

---
+++ public_html_comp1234/comp1234/lab/exercises/wk11/assets/script/juihsinwong.js
@@ -0,0 +1,7 @@
+// let num = prompt("Enter a number: ");
+// document.write("<br>" + num);
+// document.write("<br>" + (num % 2));
+
+let total = a + b + 4;
+console.log(a + b + 4);
+console.log(total);
