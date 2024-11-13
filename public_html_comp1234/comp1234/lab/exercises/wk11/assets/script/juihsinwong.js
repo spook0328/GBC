621d3ff4c3237777e5f9c637ac3612e86abd556c
Eric <eric032807@gmial.com>
2024-11-13 18:33:35 -0500

    wk11 lab exercise done

--- public_html_comp1234/comp1234/lab/exercises/wk11/assets/script/juihsinwong.js
+++ public_html_comp1234/comp1234/lab/exercises/wk11/assets/script/juihsinwong.js
@@ -1,7 +1,22 @@
-// let num = prompt("Enter a number: ");
-// document.write("<br>" + num);
-// document.write("<br>" + (num % 2));
+let name = prompt("Enter your name:", "Jui Hsin Wong");
+let id = prompt("Enter your student ID:", "101559700");
+document.write("<h1>" + name + " & " + id + "</h1>");
 
-let total = a + b + 4;
-console.log(a + b + 4);
-console.log(total);
+let num = 8;
+let num2 = 7;
+let sum = num + num2;
+let p = " your lucky number is ";
+let total = "Total";
+document.write("<h2>" + total + ":" + " </h2>");
+document.write("<p>" + name + p + sum + " </p>");
+
+// let Name = "JuiHsin Wong";
+// let id = "101559700";
+// let h2 = "Total";
+// document.write("<h1>" + Name + " & " + id + "</h1>");
+// document.write("<h2>" + h2 + ": </h2>");
+
+// let num = parseInt(prompt("Enter a number:"));
+// let num2 = parseInt(prompt("Enter a number:"));
+// document.write(s1);
+// document.write(num + num2);
