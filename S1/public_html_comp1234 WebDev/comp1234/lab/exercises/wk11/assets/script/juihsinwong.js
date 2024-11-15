e33bc707472adb38f24b793e2f2109e5ae604313
Eric <eric032807@gmail.com>
2024-11-14 22:12:48 -0500

    wk11 lab fix

--- S1/public_html_comp1234 WebDev/comp1234/lab/exercises/wk11/assets/script/juihsinwong.js
+++ S1/public_html_comp1234 WebDev/comp1234/lab/exercises/wk11/assets/script/juihsinwong.js
@@ -1,17 +1,22 @@
-let name = prompt("Enter your name:", "Jui Hsin Wong");
-let id = prompt("Enter your student ID:", "101559700");
-document.write("<h1>" + name + " & " + id + "</h1>");
+let name = prompt("Enter your name:");
+console.log(name);
+// let id = prompt("Enter your student ID:", "101559700");
+let id = "101559700";
+console.log(id);
+document.write("<h1>" + "Jui Hsin Wong" + " & " + id + "</h1>");
 
 let num = 8;
 let num2 = 7;
 let sum = num + num2;
+console.log(num, num2, sum);
 let p = " your lucky number is ";
 let total = "Total";
 document.write("<h2>" + total + ":" + " </h2>");
 document.write("<p>" + name + p + sum + " </p>");
 
+console.log();
 // let Name = "JuiHsin Wong";
-// let id = "101559700";
+
 // let h2 = "Total";
 // document.write("<h1>" + Name + " & " + id + "</h1>");
 // document.write("<h2>" + h2 + ": </h2>");
