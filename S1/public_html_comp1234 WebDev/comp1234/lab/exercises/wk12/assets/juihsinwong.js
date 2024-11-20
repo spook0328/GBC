dc09b065599b45dd4f7b61dc2196673cb598dc41
Eric <eric032807@gmial.com>
2024-11-20 15:05:44 -0500

    wk12 build exercise12

---
+++ S1/public_html_comp1234 WebDev/comp1234/lab/exercises/wk12/assets/juihsinwong.js
@@ -0,0 +1,23 @@
+document.write("<h1>" + "Simple Calculator" + "</h1>");
+document.write("<h2>" + "JuiHsin Wong" + " and 101559700" + "</h2>");
+
+let operation = prompt(
+  "Menu\n" + "1.(+)\n" + "2.(-)\n" + "3.(*)\n" + "4.(/)\n"
+);
+let num1 = Number(prompt("Enter your first number:"));
+let num2 = Number(prompt("Enter your second number: "));
+let total = 0;
+
+if (operation == 1) {
+  total = num1 + num2;
+  document.write("<h3>" + num1 + " + " + num2 + " = " + total + "</h3>");
+} else if (operation == 2) {
+  total = num1 - num2;
+  document.write("<h3>" + num1 + " - " + num2 + " = " + total + "</h3>");
+} else if (operation == 3) {
+  total = num1 * num2;
+  document.write("<h3>" + num1 + " * " + num2 + " = " + total + "</h3>");
+} else {
+  total = num1 / num2;
+  document.write("<h3>" + num1 + " / " + num2 + " = " + total + "</h3>");
+}
