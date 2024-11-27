e7e3923c669816ce1498a1e774a1f8210bc3e1ec
Eric <eric032807@gmail.com>
2024-11-27 00:34:17 -0500

    assignment3 part4

--- S1/comp1234 WebDev/comp1234/assignments/assignment03/assignment3/101559700.js
+++ S1/comp1234 WebDev/comp1234/assignments/assignment03/assignment3/101559700.js
@@ -12,23 +12,23 @@ function part1() {
   }
 
   document.write(`
-      <div style="
-        position: fixed;
-        bottom: 10px;
-        right: 10px;
-        padding: 10px;
-    ">
-    <b>Student ID :</b> 101559700 <b>Full name:</b> Jui-Hsin Wong <br>
-    <b>Professor's name:</b> Professor Leonard <b>Lab session day:</b> Wednesday
-    <br><br>
-    <b>Encoded Student ID :</b> ${encoded(
-      101559700
-    )} ; <b>Encoded Full name:</b> ${encoded("JuiHsinWong")}  <br>
-    <b>Encoded Professor's name:</b> ${encoded(
-      "ProfessorLeonard"
-    )}  ; <b>Encoded Lab session day:</b> ${encoded("Wednesday")} 
-    </div>
-  `);
+        <div style="
+          position: fixed;
+          bottom: 10px;
+          right: 10px;
+          padding: 10px;
+      ">
+      <b>Student ID :</b> 101559700 <b>Full name:</b> Jui-Hsin Wong <br>
+      <b>Professor's name:</b> Professor Leonard <b>Lab session day:</b> Wednesday
+      <br><br>
+      <b>Encoded Student ID :</b> ${encoded(
+        101559700
+      )} ; <b>Encoded Full name:</b> ${encoded("JuiHsinWong")}  <br>
+      <b>Encoded Professor's name:</b> ${encoded(
+        "ProfessorLeonard"
+      )}  ; <b>Encoded Lab session day:</b> ${encoded("Wednesday")}
+      </div>
+    `);
 }
 
 function part2(num1, num2) {
@@ -46,6 +46,7 @@ function part2(num1, num2) {
   } else {
     _return = 0;
   }
+  //console.log(_return);
   /* Your code ends here.
        Don't add or change anything after this line.*/
   return _return;
@@ -76,7 +77,7 @@ function part3(startNum, endNum) {
   if (sum % 3 == 0) {
     _return = "Magic" + _return;
   }
-  console.log(_return);
+  //   console.log(_return);
   /* Your code ends here.
        Don't add or change anything after this line.*/
   return _return;
@@ -86,6 +87,46 @@ function part4(array_index, array) {
   // Assign the return value to a variable named _return
   let _return = "";
   // Your code should start here
+
+  //check array_index
+  if (array_index < 0 || array_index >= array.length) {
+    return false;
+  }
+
+  //EVEN
+  if (array_index % 2 == 0) {
+    let sum = 0;
+    for (let i = 0; i < array_index; i++) {
+      sum += array[i];
+    }
+    _return = sum;
+  }
+
+  //ODD
+  if (array_index % 2 == 1) {
+    let sum = 0;
+    for (let i = array_index + 1; i < array.length; i++) {
+      sum += array[i];
+    }
+    _return = sum;
+  }
+
+  //   part4(0, [])
+  if (array.length === 0) {
+    _return = false;
+  }
+
+  //   part4(2, [1, 2, 0, 3, 4])
+  if (array[array_index] == 0) {
+    _return = true;
+  }
+
+  //part4(0, [2, 1, 3])
+  //part4(2, [1, 2, 3])
+  if ((array_index = 0 || array_index == array.length - 1)) {
+    _return = 0;
+  }
+  //console.log(_return);
 
   /* Your code ends here.
        Don't add or change anything after this line.*/
