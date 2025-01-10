6f7d27a2c47472357c31c49a476fe1a2eac7d26f
Eric <eric032807@gmial.com>
2025-01-10 14:54:04 -0500

    commp1235 add

---
+++ S2/comp 1235 Full Steck/labs/wk1/walkthrough1/walthrough1.js
@@ -0,0 +1,36 @@
+alert("Welcome to the website");
+console.log("This space is for debugging");
+
+// The datatype of first_name is: string
+let first_name = prompt("Please enter your name");
+// Concatenation with the + symbol
+document.write("Your name is: " + first_name);
+
+// Double/float
+let price;
+price = null;
+document.write("<br>The price is: " + price + "<br>");
+
+// Constants - this value is not changable
+const TAX = 0.13;
+
+// Integer value
+let age = 17;
+
+// Boolean
+let is_registered = true;
+
+// Array - the values are changable even though it's a const
+const friends = ["Lee", "Sarah", "Ali", "Abbas"];
+
+if((age !== 5 || age > 18) && age < 80) {
+    document.write("<br>You are of legal age, but a young person<br>");
+} else if (age > 13){
+    document.write("<br>You are a teenager<br>");
+} else if(age === 5){
+    document.write("You are 5 years old");
+} else if(age !== 5){
+    document.write("You are not 5 years old");
+} else {
+    document.write("<br>You are a child<br>");
+}
