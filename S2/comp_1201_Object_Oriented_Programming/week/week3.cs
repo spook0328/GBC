90dac3320a71519afb8cf66fc75edf4381dc3a71
Eric <eric032807@gmail.com>
2025-01-23 11:34:55 -0500

    wk3 different IDE

--- S2/comp_1201_Object_Oriented_Programming/week/week3.cs
+++ S2/comp_1201_Object_Oriented_Programming/week/week3.cs
@@ -29,18 +29,69 @@ namespace consoleApp1
 
         static void Main(string[] args)
         {
-            Testing d = new Testing()//you need to create an instance (object) from the class
+            Testing d = new Testing();//you need to create an instance (object) from the class
             d.Message(); //non-static
             Testing.Message();
             
-            Testing.Message2()// static
+            Testing.Message2(); // static
             console.WriteLine("Hello, world!");
 
             //--------------------
             //Guesing Game, in this game user try to guess a random number
-            //between 1 to 100. the game provide feedbacks to the user till find
-            //the number
-        }
+            //between 1 to 100. the game provide feedbacks to the user till find that number
+            //the game provides hints to the user till the number is found!
+
+            //then the game indicates how many times took for the user to guess
+            //the number! if the input number is bigger than random number then 
+            //computer provide feedback such as guess something smaller and ask again and 
+            //if the input number is smaller than random number then 
+            //computer provide feedback such as guess something bigger and ask again 
+            // ------------------
+            //Alter the game to two player game which the player alternatively try to find the 
+            // random number. Whoever find the number first win the game!
+            //1) collect the user names!
+            // hint : have a variable called 'turn' which keeps track  of the turn of players
+            // extra hint : you need to change the turn after each guess!
+            // EXTRA : since player 1 has dasadvantage over player 2 is better to start who begins the game
+            //random...
+
+
+           int randNum = new Random(). Next(1, 101);
+           int userInput;
+           int counter = 0;
+           int lower = 1;
+           int upper = 100;
+           while (true)
+           {
+                console.WriteLine("Enter a number between {lower} to {upper}");
+                userInput = Convert.ToInt32(console.ReadLine()); counter++;
+
+                if (randNum > userInput) { Console.WriteLine("guess something bigger!");
+                    if (userInput < lower) lower = userInput; }
+                else if (randNum < userInput) { console.WriteLine("guess somthing smaaler!"); 
+                    if (userInput > upper) upper = userInput; }
+                else {
+                    Console.WriteLine("Bingo, it took you {counter} time to guess it");break;
+                }
+           }
+
+        //    console.WriteLine("Enter a number between 1 to 100");
+        //    int userInput = Convert.ToInt32(console.ReadLine());
+
+        //    //ranNum > userInput , userInput > ranNum , userInput == ranNum
+        //    while(userInput != randNum){
+
+        //     if (randNum > userInput){
+        //         console.WriteLine("guess something bigger!");
+        //     }else { console.WriteLine("guess something smaller!");}
+
+        //     consoleApp1.WriteLine("Enter a number between 1 to 100");
+        //     userInput = Convert.ToInt32(console.ReadLine());
+
+
+        //    }
+        //    Console.WriteLine("Bingo");
+        // }
     }
 
 }
