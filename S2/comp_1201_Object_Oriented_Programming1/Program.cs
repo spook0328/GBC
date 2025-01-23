0326cd69581da8984f97a5aa57d5b8c04ddc7cd5
Eric <eric032807@gmail.com>
2025-01-23 18:43:37 -0500

    fix com1202 and upload

---
+++ S2/comp_1201_Object_Oriented_Programming1/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace comp_1201_Object_Oriented_Programming1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //week1.week1Main();
+        //week2.week1Main();
+        //week3.week1Main();
+    }
+}
