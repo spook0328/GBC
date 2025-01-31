465eeb90a037fd6a61d9b2a9b47637295790dd01
Eric <eric032807@gmail.com>
2025-01-31 13:36:47 -0500

    full stack wk4 lab

--- S2/comp_1201_Object_Oriented_Programming1/obj/Debug/net8.0/comp_1201_Object_Oriented_Programming1.AssemblyInfo.cs
+++ S2/comp_1201_Object_Oriented_Programming1/obj/Debug/net8.0/comp_1201_Object_Oriented_Programming1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("comp_1201_Object_Oriented_Programming1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd94ddbf4b5704cb83c48c5568d10c7587a57f56")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+043a3704604d50751cdc2146335174dc0e38b0c4")]
 [assembly: System.Reflection.AssemblyProductAttribute("comp_1201_Object_Oriented_Programming1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("comp_1201_Object_Oriented_Programming1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
