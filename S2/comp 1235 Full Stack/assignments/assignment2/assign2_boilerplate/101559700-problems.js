9692e58bdccc09295387a64f97fc9781a2f7f089
Eric <eric032807@gmail.com>
2025-02-14 01:59:34 -0500

    Comp1235 assignment2 Q1

--- S2/comp 1235 Full Stack/assignments/assignment2/assign2_boilerplate/101559700-problems.js
+++ S2/comp 1235 Full Stack/assignments/assignment2/assign2_boilerplate/101559700-problems.js
@@ -1,5 +1,16 @@
 "use strict";
 
-function _checkS(password){
+function _checkS(password) {
+  return password.indexOf("*");
+}
 
+function _findNumOfDigits(arg) {
+  let count = 0;
+  let str = String(arg);
+  for (let i = 0; i < str.length; i++) {
+    if (/^\d$/.test(str[i])) {
+      count++;
+    }
+  }
+  return count;
 }
--- S2/comp 1235 Full Stack/assignments/assignment2/assign2_boilerplate/tests.js
+++ S2/comp 1235 Full Stack/assignments/assignment2/assign2_boilerplate/tests.js
@@ -3,11 +3,23 @@
 const chai = window.chai;
 const expect = chai.expect;
 
-describe('_checkS', () =>  {
-    it('Check the index of * in the password',  () => {
-        expect(_checkS("ibntij53*")).to.deep.equal(6)
-    })
-})
+describe("_checkS", () => {
+  it("Check the index of * in the password", () => {
+    expect(_checkS("ibntij53*")).to.deep.equal(8);
+  });
+});
+
+//Q1
+describe("_findNumOfDigits", function () {
+  it("Should returns the count of the number of digits back to the caller", function () {
+    expect(_findNumOfDigits(1000)).to.equal(4);
+    expect(_findNumOfDigits("abcd")).to.equal(0);
+    expect(_findNumOfDigits(12)).to.equal(2);
+    expect(_findNumOfDigits("COMP1235")).to.equal(4);
+    expect(_findNumOfDigits(0)).to.equal(1);
+    expect(_findNumOfDigits("C1O2M3P5")).to.equal(4);
+  });
+});
 
 /***************************************************************************************
 describe('yourFunctionName', () =>  {
