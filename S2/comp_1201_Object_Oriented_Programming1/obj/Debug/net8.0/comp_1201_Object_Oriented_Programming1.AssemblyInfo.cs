234d6641da2da828a18c5887fdb25943c801d225
Eric <eric032807@gmail.com>
2025-02-15 15:42:08 -0500

    comp1235 assignment2 final

--- S2/comp_1201_Object_Oriented_Programming1/obj/Debug/net8.0/comp_1201_Object_Oriented_Programming1.AssemblyInfo.cs
+++ S2/comp_1201_Object_Oriented_Programming1/obj/Debug/net8.0/comp_1201_Object_Oriented_Programming1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("comp_1201_Object_Oriented_Programming1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+87a9686a572b51d64c4872e4a52ec4e23282784f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b9a9318be7b2d37f64793036728ecf4e19c5f397")]
 [assembly: System.Reflection.AssemblyProductAttribute("comp_1201_Object_Oriented_Programming1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("comp_1201_Object_Oriented_Programming1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
