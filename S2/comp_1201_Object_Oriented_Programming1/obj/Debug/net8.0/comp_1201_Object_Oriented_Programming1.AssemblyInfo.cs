4bbcff4806ebdff91e562d8a30cb89beee86d1b7
Eric <eric032807@gmail.com>
2025-03-13 15:47:41 -0400

    gitignore renew

--- S2/comp_1201_Object_Oriented_Programming1/obj/Debug/net8.0/comp_1201_Object_Oriented_Programming1.AssemblyInfo.cs
+++ S2/comp_1201_Object_Oriented_Programming1/obj/Debug/net8.0/comp_1201_Object_Oriented_Programming1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("comp_1201_Object_Oriented_Programming1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc6c8c6108ba1b73fdfabeb564b196229571e4d8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2239ccca5001c69d9146b5a0e4baeb42828bef73")]
 [assembly: System.Reflection.AssemblyProductAttribute("comp_1201_Object_Oriented_Programming1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("comp_1201_Object_Oriented_Programming1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
