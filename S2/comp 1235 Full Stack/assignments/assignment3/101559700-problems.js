65225f1fedff410c399aa614b187d72b4cb5307d
Eric <eric032807@gmail.com>
2025-03-16 20:49:49 -0400

    comp1235 q2 q3

--- S2/comp 1235 Full Stack/assignments/assignment3/101559700-problems.js
+++ S2/comp 1235 Full Stack/assignments/assignment3/101559700-problems.js
@@ -1,14 +1,42 @@
 "use strict";
 function Circle(radius) {
-  //Circumference formula 2 pi r
-  // area formula pi r*r
   if (radius <= 0 || typeof radius != "number") {
-    console.log("Error parameter!");
+    throw new Error("Error parameter!");
   }
+  this.radius = radius;
 
-  this.area = function (params) {
-    return Math.PI * radius * radius;
+  this.area = function () {
+    return parseFloat((Math.PI * this.radius * this.radius).toFixed(2));
+  };
+  this.perimeter = function () {
+    return parseFloat((2 * Math.PI * this.radius).toFixed(2));
   };
 }
-const ACircle = new Circle(5);
-console.log("Area: ", ACircle.area());
+
+const calculateSalary = (array) => {
+  if (array.length > 7) {
+    throw new Error("Error parameter!");
+  }
+  if (array.some((h) => h < 0)) {
+    throw new Error("Error parameter!");
+  }
+  return array.reduce((sum, h, d) => {
+    const weekend = d >= 5;
+    const basehours = Math.min(h, 8);
+    const extrahours = Math.max(h - 8, 0);
+
+    let dailyPay = basehours * 10 + extrahours * 15;
+    if (weekend) dailyPay *= 2;
+
+    return sum + dailyPay;
+  }, 0);
+};
+
+const indexMultipler = (array) => {
+  return array.reduce((sum, value, index) => {
+    return sum + value * index;
+  }, 0);
+};
+function filteredJSON([array]) {}
+
+function breakAway(array, n) {}
--- S2/comp 1235 Full Stack/assignments/assignment3/tests.js
+++ S2/comp 1235 Full Stack/assignments/assignment3/tests.js
@@ -3,19 +3,45 @@
 const chai = window.chai;
 const expect = chai.expect;
 
-Describe("Circle", () => {
-  it("should calculate correct area and perimeter for radius 3", () => {
-    const c = Circle(3);
+describe("Circle", () => {
+  it("should return 28.27 & 18.85", () => {
+    const c = new Circle(3);
     expect(c.area()).to.deep.equal(28.27);
     expect(c.perimeter()).to.deep.equal(18.85);
   });
-  it("should calculate correct area and perimeter for radius 7", () => {
-    expect(c.area()).to.deep.equal(153.97);
+  it("should return 153.94 & 43.98", () => {
+    const c = new Circle(7);
+    expect(c.area()).to.deep.equal(153.94);
     expect(c.perimeter()).to.deep.equal(43.98);
   });
-  it("should calculate correct area and perimeter for radius 9", () => {
+  it("should return 254.47 & 56.55", () => {
+    const c = new Circle(9);
     expect(c.area()).to.deep.equal(254.47);
     expect(c.perimeter()).to.deep.equal(56.55);
+  });
+});
+
+describe("calculateSalary", () => {
+  it("should return 400", () => {
+    expect(calculateSalary([8, 8, 8, 8, 8, 0, 0])).to.equal(400);
+  });
+  it("should return 410", () => {
+    expect(calculateSalary([10, 10, 10, 0, 8, 0, 0])).to.equal(410);
+  });
+  it("should return 280", () => {
+    expect(calculateSalary([0, 0, 0, 0, 0, 12, 0])).to.equal(280);
+  });
+});
+
+describe("indexMultipler", () => {
+  it("should return 40", () => {
+    expect(indexMultipler([1, 2, 3, 4, 5])).to.equal(40);
+  });
+  it("should return -2", () => {
+    expect(indexMultipler([-3, 0, 8, -6])).to.equal(-2);
+  });
+  it("should return -34", () => {
+    expect(indexMultipler([15, 16, -100, 50])).to.equal(-34);
   });
 });
 /***************************************************************************************
