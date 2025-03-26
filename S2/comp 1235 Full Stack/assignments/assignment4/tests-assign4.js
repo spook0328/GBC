c888113e70f6f6467dd237ccda9b51561dab44ce
Eric <eric032807@gmail.com>
2025-03-26 08:18:35 -0400

    Comp1235 assignment4

---
+++ S2/comp 1235 Full Stack/assignments/assignment4/tests-assign4.js
@@ -0,0 +1,22 @@
+"use strict";
+
+const chai = window.chai;
+const expect = chai.expect;
+
+/***************************************************************************************
+ Testing TEMPLATES
+
+ //To test a regular function
+ describe('yourFunctionName', () =>  {
+    it('please enter your function description here',  () => {
+        expect(yourFunctionName(parameters)).to.deep.equal(expected_result);
+    })
+})
+
+ //To test async/await functions
+ describe('yourFunctionName()', () =>  {
+    it('The async/await function description',  async () => {
+          expect(await yourFunctionName()).to.deep.equal(expected_result);
+    })
+})
+ ***************************************************************************************/
