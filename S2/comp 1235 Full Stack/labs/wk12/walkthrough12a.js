aab3cf36d7f6307dd244eeeeee184bd887e2c2a6
Eric <eric032807@gmail.com>
2025-03-28 13:45:52 -0400

    Full stack lab11

---
+++ S2/comp 1235 Full Stack/labs/wk12/walkthrough12a.js
@@ -0,0 +1,10 @@
+// define a var a
+let a = 10;
+console.log(a);
+// define a const
+const b = "hello";
+console.log(b);
+
+function add(a, b) {
+  return a + b;
+}
---
+++ S2/comp 1235 Full Stack/labs/wk12/walkthrough12b.js
@@ -0,0 +1,33 @@
+//Step 1: Define the function
+function Img(title, src, content) {
+  this.title = title;
+  this.src = src;
+  this.content = content;
+}
+
+//step 2: Create the Markup
+Img.prototype.makeMarkup = function () {
+  return `<div> <img src ="＄{this.src}" 
+    content ="${this.content}"
+    title = "${this.title}"
+    >
+    </div>`;
+};
+
+//Step3 : Load the File/Content with Dom element
+Img.prototype.makeElement = function () {
+  let ImgDiv = document.createElement("div");
+  ImgDiv.innerHTML = this.makeMarkup();
+  return ImgDiv;
+};
+
+//Step 4: Load the page addEventListener
+document.addEventListener("DOMContentLoaded", () => {
+  const bodyElement = document.querySelector("body");
+  const i1 = new Img(
+    "GBC",
+    "https://learn.georgebrown.ca/d2l/lp/navbars/6606/theme/viewimage/1154/view?v=20.25.3.18517",
+    "This is the Content"
+  );
+  bodyElement.appendChild(i1.makeElement());
+});
